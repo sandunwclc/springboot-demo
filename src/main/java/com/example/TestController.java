82568cdda511b27eed828f900c875d7c6cdb5299
Sandun Abeyweera <srabeyweera@gmail.com>
2025-07-03 19:47:56 -0500

    Initial commit

--- src/main/java/com/example/TestController.java
+++ src/main/java/com/example/TestController.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,11 @@ public class TestController {
 	@GetMapping
 	public String hello() {
 		return "helloworld!!";
+	}
+	
+	@PostMapping
+	public String post(@RequestBody String username) {
+		System.out.println("username: "+username);
+		return "helloworld!! "+username;
 	}
 }
